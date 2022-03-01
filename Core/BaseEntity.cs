fbf6917854c5e2fcdd7cdf5624822f58afe521a3
numanakoluk <numanakoluk01@gmail.com>
2022-03-01 12:35:05 +0300

    Entity Class

---
+++ Core/BaseEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    //Abstract yapma sebebim instance alınmasın newlenemesin.   
+    public abstract class BaseEntity
+    {
+        public int Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdateDate { get; set; }
+    }
+}
