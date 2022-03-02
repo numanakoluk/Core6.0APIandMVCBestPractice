60be5af028b6957b5d358c3c201c8836c6a76d9b
numanakoluk <numanakoluk01@gmail.com>
2022-03-02 15:42:54 +0300

    IService

---
+++ Core/IService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    public interface IService<T> where T : class
+    {
+        Task<T> GetByIdAsyn(int id);
+       
+        Task<IEnumerable<T>> GetAllAsyn();
+        IQueryable<T> Where(Expression<Func<T, bool>> expression);
+
+        Task<bool> AnyAsync(Expression<Func<T, bool>> expression);
+        Task<T> AddAsync(T entity);
+
+        Task<T> AddRangeAsyn(IEnumerable<T> entities);
+
+        Task UpdateAsync(T entity);
+        Task RemoveAsync(T entity);
+
+        Task<T> RemoveRangeAsyn(IEnumerable<T> entities);
+        
+
+    }
+}
