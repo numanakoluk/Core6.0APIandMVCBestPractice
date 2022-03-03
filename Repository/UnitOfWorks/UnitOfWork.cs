34a258716515eaae5d002781df181881cb36ed71
numanakoluk <numanakoluk01@gmail.com>
2022-03-04 00:48:27 +0300

    UnitOfWork Imp

---
+++ Repository/UnitOfWorks/UnitOfWork.cs
@@ -0,0 +1,30 @@
+﻿using Core.UnitOfWorks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.UnitOfWorks
+{
+    public class UnitOfWork : IUnitOFWork
+    {
+        private readonly AppDbContext _context;
+
+        //Ctor Desc
+        public UnitOfWork(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Commit()
+        {
+            _context.SaveChanges();
+        }
+
+        public async Task CommitAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
