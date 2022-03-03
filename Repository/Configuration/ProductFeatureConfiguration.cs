5da3d5735c9c09528c143232d3491bcda2c476a9
numanakoluk <numanakoluk01@gmail.com>
2022-03-03 13:34:15 +0300

    EntityConfiguration(Product and ProductFeature)

---
+++ Repository/Configuration/ProductFeatureConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Core;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Configuration
+{
+    internal class ProductFeatureConfiguration : IEntityTypeConfiguration<ProductFeature>
+    {
+        public void Configure(EntityTypeBuilder<ProductFeature> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id).UseIdentityColumn();
+
+            //Relationship
+            builder.HasOne(x => x.Product).WithOne(x => x.ProductFeature).HasForeignKey<ProductFeature>(x => x.ProductId);
+        }
+    }
+}
