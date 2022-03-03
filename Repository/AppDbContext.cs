c01f1461ce603137c1484a0c0a816ec650017388
numanakoluk <numanakoluk01@gmail.com>
2022-03-03 12:42:40 +0300

    AppDbContext

---
+++ Repository/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Core;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class AppDbContext:DbContext
+    {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<ProductFeature> ProductFeatures { get; set; }
+    }
+}
