1a822623f3bdeca88dafcd42f8617ad6f9417833
numanakoluk <numanakoluk01@gmail.com>
2022-03-05 18:26:27 +0300

    CustomRepsonseDTO

---
+++ Core/DTOs/CustomResponseDto.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Core.DTOs
+{
+    public class CustomResponseDto<T>
+    {
+        public T Data { get; set; }
+
+        [JsonIgnore]
+        public int StatusCode { get; set; }
+        public List<String> Errors { get; set; }
+
+        public static CustomResponseDto<T> Success(int statusCode, T data)
+        {
+            //Error default already null but still writed
+            return new CustomResponseDto<T> { Data = data, StatusCode = statusCode};
+        }
+
+        public static CustomResponseDto<T> Success(int statusCode)
+        {
+            //Error default already null but still writed
+            return new CustomResponseDto<T> { StatusCode = statusCode};
+        }
+
+        public static CustomResponseDto<T> Fail(int statusCode, List<string> errors)
+        {
+            //Error default already null but still writed
+            return new CustomResponseDto<T> { StatusCode = statusCode, Errors=errors};
+        }
+
+        public static CustomResponseDto<T> Fail(int statusCode,string error)
+        {
+            //Error default already null but still writed
+            return new CustomResponseDto<T> { StatusCode = statusCode, Errors =  new List<string> { error } };
+        }
+    }
+}
