57467bf80d06b1b71fb35400ead9ad6198ae14d3
numanakoluk <numanakoluk01@gmail.com>
2022-03-05 14:57:26 +0300

    Mapping

---
+++ Service/Mapping/MapProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using Core.DTOs;
+using Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Mapping
+{
+    //Add MVC And API just Project Public
+    public class MapProfile:Profile
+    {
+        public MapProfile()
+        {
+            CreateMap<Product, ProductDto>().ReverseMap(); //JUST THE opposite
+            CreateMap<Category, CategoryDto>().ReverseMap();
+            CreateMap<ProductFeature, ProductFeatureDto>().ReverseMap();
+            CreateMap<ProductDto, Product>(); //Update Entity
+        }
+    }
+}
