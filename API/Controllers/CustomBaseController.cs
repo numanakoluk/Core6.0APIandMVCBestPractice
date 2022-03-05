444599f1014df848c928ba0d03fa129b2755cdc8
numanakoluk <numanakoluk01@gmail.com>
2022-03-05 19:29:57 +0300

    CustomBaseController

---
+++ API/Controllers/CustomBaseController.cs
@@ -0,0 +1,27 @@
+﻿using Core.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomBaseController : ControllerBase
+    {
+        //For Ok NoContent, Created BadRequest
+
+        public IActionResult CreateActionResult<T>(CustomResponseDto<T> response)
+        {
+            if (response.StatusCode == 204)
+                return new ObjectResult(null)
+                {
+                    StatusCode = response.StatusCode
+                };
+            return new ObjectResult(response)
+            {
+                StatusCode = response.StatusCode
+            };
+            
+        }
+    }
+}
---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using Core.DTOs;
+using Core.Models;
+using Core.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+        private readonly IService<Product> _service;
+
+        public ProductsController(IService<Product> service, IMapper mapper)
+        {
+            _service = service;
+            _mapper = mapper;
+        }
+        public async Task<IActionResult> All()
+        {
+            var products = await _service.GetAllAsync();
+
+            var productsDto = _mapper.Map<List<ProductDto>>(products.ToList());
+            return CustomResponseDto<List<ProductDto>>.Success(200, productsDto);
+        }
+
+    }
+}
