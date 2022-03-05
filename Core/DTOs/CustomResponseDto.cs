02bb42dbfad3283691190ff7d674a4d594436951
numanakoluk <numanakoluk01@gmail.com>
2022-03-05 18:32:15 +0300

    NoContentDto

--- Core/DTOs/CustomResponseDto.cs
+++ Core/DTOs/CustomResponseDto.cs
@@ -7,36 +7,66 @@ using System.Threading.Tasks;
 
 namespace Core.DTOs
 {
-    public class CustomResponseDto<T>
-    {
-        public T Data { get; set; }
+    //public class CustomNoResponseDto<T>
+    //{
+    //    //Nothing data but return error
+    //    [JsonIgnore]
+    //    public int StatusCode { get; set; }
+    //    public List<String> Errors { get; set; }
 
-        [JsonIgnore]
-        public int StatusCode { get; set; }
-        public List<String> Errors { get; set; }
+    //    public static CustomResponseDto<T> Success(int statusCode, T data)
+    //    {
+    //        //Error default already null but still writed
+    //        return new CustomResponseDto<T> { Data = data, StatusCode = statusCode };
+    //    }
 
-        public static CustomResponseDto<T> Success(int statusCode, T data)
+    //    public static CustomResponseDto<T> Success(int statusCode)
+    //    {
+    //        //Error default already null but still writed
+    //        return new CustomResponseDto<T> { StatusCode = statusCode };
+    //    }
+
+    //    public static CustomResponseDto<T> Fail(int statusCode, List<string> errors)
+    //    {
+    //        //Error default already null but still writed
+    //        return new CustomResponseDto<T> { StatusCode = statusCode, Errors = errors };
+    //    }
+
+    //    public static CustomResponseDto<T> Fail(int statusCode, string error)
+    //    {
+    //        //Error default already null but still writed
+    //        return new CustomResponseDto<T> { StatusCode = statusCode, Errors = new List<string> { error } };
+    //    }
+        public class CustomResponseDto<T>
         {
-            //Error default already null but still writed
-            return new CustomResponseDto<T> { Data = data, StatusCode = statusCode};
-        }
+            public T Data { get; set; }
 
-        public static CustomResponseDto<T> Success(int statusCode)
-        {
-            //Error default already null but still writed
-            return new CustomResponseDto<T> { StatusCode = statusCode};
-        }
+            [JsonIgnore]
+            public int StatusCode { get; set; }
+            public List<String> Errors { get; set; }
 
-        public static CustomResponseDto<T> Fail(int statusCode, List<string> errors)
-        {
-            //Error default already null but still writed
-            return new CustomResponseDto<T> { StatusCode = statusCode, Errors=errors};
-        }
+            public static CustomResponseDto<T> Success(int statusCode, T data)
+            {
+                //Error default already null but still writed
+                return new CustomResponseDto<T> { Data = data, StatusCode = statusCode };
+            }
 
-        public static CustomResponseDto<T> Fail(int statusCode,string error)
-        {
-            //Error default already null but still writed
-            return new CustomResponseDto<T> { StatusCode = statusCode, Errors =  new List<string> { error } };
+            public static CustomResponseDto<T> Success(int statusCode)
+            {
+                //Error default already null but still writed
+                return new CustomResponseDto<T> { StatusCode = statusCode };
+            }
+
+            public static CustomResponseDto<T> Fail(int statusCode, List<string> errors)
+            {
+                //Error default already null but still writed
+                return new CustomResponseDto<T> { StatusCode = statusCode, Errors = errors };
+            }
+
+            public static CustomResponseDto<T> Fail(int statusCode, string error)
+            {
+                //Error default already null but still writed
+                return new CustomResponseDto<T> { StatusCode = statusCode, Errors = new List<string> { error } };
+            }
         }
     }
-}
---
+++ Core/DTOs/NoContentDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DTOs
+{
+    
+    //Nothing data but return error
+    public class NoContentDto
+    {
+
+    }
+}
