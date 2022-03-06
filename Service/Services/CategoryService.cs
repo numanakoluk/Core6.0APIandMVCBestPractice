b6927319030c265a3f7c1926c8492f05acde1d49
numanakoluk <numanakoluk01@gmail.com>
2022-03-07 01:55:07 +0300

    CategoryService

---
+++ Service/Services/CategoryService.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using Core.DTOs;
+using Core.Models;
+using Core.Repositories;
+using Core.Services;
+using Core.UnitOfWorks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Services
+{
+    public class CategoryService : Service<Category>, ICategoryService
+    {
+        private readonly ICategoryRepository _categoryRepository;
+        private readonly IMapper _mapper;
+        public CategoryService(IGenericRepository<Category> repository, IUnitOFWork unitOfWork, IMapper mapper, ICategoryRepository categoryRepository) : base(repository, unitOfWork)
+        {
+            _mapper = mapper;
+            _categoryRepository = categoryRepository;
+        }
+
+        public async Task<CustomResponseDto<CategoryWithProductsDto>> GetSingleCategoryByIdWithProductsAsync(int categoryId)
+        {
+            var category = await _categoryRepository.GetSingleCategoryByIdWithProductsAsync(categoryId);
+            var categoryDto= _mapper.Map<CategoryWithProductsDto>(category);
+
+            return CustomResponseDto<CategoryWithProductsDto>.Success(200, categoryDto);
+        }
+    }
+}
