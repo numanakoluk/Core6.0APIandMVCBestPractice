52a0b79aa85a628d0f66d79404d342e92ce6555d
numanakoluk <numanakoluk01@gmail.com>
2022-03-07 01:51:27 +0300

    ICategoryService

--- Core/Services/ICategoryService.cs
+++ Core/Services/ICategoryService.cs
@@ -1,4 +1,5 @@
-﻿using Core.Models;
+﻿using Core.DTOs;
+using Core.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,7 @@ namespace Core.Services
 {
     public interface ICategoryService: IService<Category>
     {
+       public  Task<CustomResponseDto<CategoryWithProductsDto>> GetSingleCategoryByIdWithProductsAsync(int categoryId);
 
 
     }
