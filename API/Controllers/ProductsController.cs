282a270aaed66d236cc545dc3713e03c121bb0a0
numanakoluk <numanakoluk01@gmail.com>
2022-03-07 01:37:57 +0300

    CategoryCon and ICategoryRep

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -7,8 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
+   
 
     public class ProductsController : CustomBaseController
     {
