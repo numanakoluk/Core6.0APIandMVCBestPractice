db60b7d54f82e5b8b183f9aede3bf2d5fe0683b5
numanakoluk <numanakoluk01@gmail.com>
2022-03-06 22:54:01 +0300

    IProductRepository

---
+++ Core/Repositories/IProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Repositories
+{
+    //Special or customer for Product
+    public interface IProductRepository : IGenericRepository<Product>
+    {
+        Task<List<Product>> GetProductsWithCategory();
+    }
+}
