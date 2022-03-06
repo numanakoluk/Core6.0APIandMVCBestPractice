d11e19e58d60cddcd5cd46c040f6574f727bb23e
numanakoluk <numanakoluk01@gmail.com>
2022-03-06 23:02:52 +0300

    ProductWithCategoryDto

---
+++ Core/Services/IProductService.cs
@@ -0,0 +1,14 @@
+﻿using Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Services
+{
+    public interface IProductService: IService<Product>
+    {
+        Task<List<Product>> GetProductsWithCategory();
+    }
+}
