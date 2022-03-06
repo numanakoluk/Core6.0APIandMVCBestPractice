8bd3ac8458f223c42d6f62db6e59d55be46d6ce4
numanakoluk <numanakoluk01@gmail.com>
2022-03-07 02:33:49 +0300

    ProductDtoValidator

---
+++ Service/Validations/ProductDtoValidator.cs
@@ -0,0 +1,23 @@
+﻿using Core.DTOs;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Validations
+{
+    public class ProductDtoValidator: AbstractValidator<ProductDto>
+    {
+        public ProductDtoValidator()
+        {
+            RuleFor(x => x.Name).NotNull().WithMessage("{PropertyName} is required").NotEmpty().WithMessage("{PropertyName} is required");
+
+
+            RuleFor(x => x.Price).InclusiveBetween(1, int.MaxValue).WithMessage("{Price} must be greater 0");
+            RuleFor(x => x.Stock).InclusiveBetween(1, int.MaxValue).WithMessage("{Stock} must be greater 0");
+            RuleFor(x => x.CategoryId).InclusiveBetween(1, int.MaxValue).WithMessage("{CategoryId} must be greater 0");
+        }
+    }
+}
