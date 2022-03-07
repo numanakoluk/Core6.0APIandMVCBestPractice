325a8c7204bbaa1a606c03f46b9b0f90d6ee16b7
numanakoluk <numanakoluk01@gmail.com>
2022-03-07 16:52:18 +0300

    Extention Custom Middleware

--- API/Middlewares/UseCustomExceptionHandler.cs
+++ API/Middlewares/UseCustomExceptionHandler.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Diagnostics;
+﻿using Core.DTOs;
+using Microsoft.AspNetCore.Diagnostics;
+using Service.Exceptions;
+using System.Text.Json;
 
 namespace API.Middlewares
 {
@@ -14,8 +17,21 @@ namespace API.Middlewares
                 {
                     context.Response.ContentType = "application/json";
                     var exceptionFeature = context.Features.Get<IExceptionHandlerFeature>();
-                    
-                    exceptionFeature.
+
+                    //login inner with Switch
+                    var statusCode = exceptionFeature.Error switch
+                    {
+                        //default "_"
+                        ClientSideException => 400, _=> 500
+
+                    };
+                    context.Response.StatusCode = statusCode;
+
+                    var response = CustomResponseDto<NoContentDto>.Fail(statusCode, exceptionFeature.Error.Message);
+
+                    //Json Cast normal to return Json but My do middleware because cast json
+                    await context.Response.WriteAsync(JsonSerializer.Serialize(response));
+
 
                 });
             });
