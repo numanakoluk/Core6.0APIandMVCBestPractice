f809befb8dd6f2262f8a18994e54e83dc26b4012
numanakoluk <numanakoluk01@gmail.com>
2022-03-07 16:43:29 +0300

    ClientSideExceptions

---
+++ API/Middlewares/UseCustomExceptionHandler.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+
+namespace API.Middlewares
+{
+    public static class UseCustomExceptionHandler
+    {
+        //write for IApplicationbuilder 
+        public static void UseCustomException(this IApplicationBuilder app)
+        {
+            app.UseExceptionHandler(config =>
+            {
+                //run finishing type middleware
+                config.Run(async context =>
+                {
+                    context.Response.ContentType = "application/json";
+                    var exceptionFeature = context.Features.Get<IExceptionHandlerFeature>();
+                    
+                    exceptionFeature.
+
+                });
+            });
+        }
+    }
+}
