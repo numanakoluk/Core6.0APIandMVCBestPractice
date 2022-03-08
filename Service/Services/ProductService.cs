e1c225eb6f4adf9df3466a735d6fb385bc0d60e1
numanakoluk <numanakoluk01@gmail.com>
2022-03-08 15:59:36 +0300

    ProductServiceNameChange

--- Service/Services/ProductService.cs
+++ Service/Services/ProductService.cs
@@ -12,11 +12,11 @@ using System.Threading.Tasks;
 
 namespace Service.Services
 {
-    public class ProductServiceWithNoCaching : Service<Product>, IProductService
+    public class ProductService : Service<Product>, IProductService
     {
         private readonly IProductRepository _productRepository;
         private readonly IMapper _mapper;
-        public ProductServiceWithNoCaching(IGenericRepository<Product> repository, IUnitOFWork unitOfWork, IMapper mapper, IProductRepository productRepository) : base(repository, unitOfWork)
+        public ProductService(IGenericRepository<Product> repository, IUnitOFWork unitOfWork, IMapper mapper, IProductRepository productRepository) : base(repository, unitOfWork)
         {
             _productRepository = productRepository;
             _mapper = mapper;
---
+++ Web/Controllers/ProductsController.cs
@@ -0,0 +1,20 @@
+﻿using Core.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _productService.GetProductsWithCategory());
+        }
+    }
+}
