41b228680bf435de8c1b36774e61df10eda2d82e
numanakoluk <numanakoluk01@gmail.com>
2022-03-08 16:59:07 +0300

    ProgramCs Conf

---
+++ Web/Modules/RepoServiceModul.cs
@@ -0,0 +1,55 @@
+﻿using Autofac;
+using Core.Repositories;
+using Core.Services;
+using Core.UnitOfWorks;
+using Repository;
+using Repository.Repositories;
+using Repository.UnitOfWorks;
+using Service.Mapping;
+using Service.Services;
+using System.Reflection;
+using Module = Autofac.Module;
+
+namespace Web.Modules
+{
+    public class RepoServiceModul:Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            //GenericRepo
+            builder.RegisterGeneric(typeof(GenericRepository<>)).As(typeof(IGenericRepository<>)).InstancePerLifetimeScope();
+
+            //GenericService
+            builder.RegisterGeneric(typeof(Service<>)).As(typeof(IService<>)).InstancePerLifetimeScope();
+
+            //Normal NotGenericUnitOfWork
+            builder.RegisterType<UnitOfWork>().As<IUnitOFWork>();
+
+
+
+            //First Take Assembly
+            var apiAssembly = Assembly.GetExecutingAssembly();
+
+            //AppDbContext Data always member
+            var repoAssembly = Assembly.GetAssembly(typeof(AppDbContext));
+
+            //Service 
+            var serviceAssembly =Assembly.GetAssembly(typeof(MapProfile));
+
+
+            //Repo Take
+
+            builder.RegisterAssemblyTypes(apiAssembly, repoAssembly, serviceAssembly).Where(x=>x.Name.EndsWith("Repository")).AsImplementedInterfaces().InstancePerLifetimeScope();
+            //InstancePerLifetimeScope => Scope(AspNetCore)
+            //InstancePerDependency => Transient
+
+            //Service take
+            builder.RegisterAssemblyTypes(apiAssembly, repoAssembly, serviceAssembly).Where(x => x.Name.EndsWith("Service")).AsImplementedInterfaces().InstancePerLifetimeScope();
+
+
+            //Cach hasn't
+
+
+        }
+    }
+}
--- Web/Program.cs
+++ Web/Program.cs
@@ -1,7 +1,34 @@
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Repository;
+using Service.Mapping;
+using System.Reflection;
+using Web.Modules;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+//AutoMapp
+builder.Services.AddAutoMapper(typeof(MapProfile));
+//AddMigration
+builder.Services.AddDbContext<AppDbContext>(x =>
+{
+    x.UseSqlServer(builder.Configuration.GetConnectionString("SqlConnection"), option =>
+    {
+        //Not Type Safe
+        //option.MigrationsAssembly("Repository") 
+        //Type Scrript Dynamic
+        option.MigrationsAssembly(Assembly.GetAssembly(typeof(AppDbContext)).GetName().Name);
+    });
+});
+
+//Autofac(DI)
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
+//Modul Activate
+builder.Host.ConfigureContainer<ContainerBuilder>(containerBuilder => containerBuilder.RegisterModule(new RepoServiceModul()));
 
 var app = builder.Build();
 
