918c129ae289e8b436810da95191f9d192f70c3a
numanakoluk <numanakoluk01@gmail.com>
2022-03-08 18:31:08 +0300

    Save

--- Web/Controllers/ProductsController.cs
+++ Web/Controllers/ProductsController.cs
@@ -1,20 +1,58 @@
-﻿using Core.Services;
+﻿using AutoMapper;
+using Core.DTOs;
+using Core.Models;
+using Core.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Web.Controllers
 {
     public class ProductsController : Controller
     {
         private readonly IProductService _productService;
+        private readonly ICategoryService _categoryService;
+        private readonly IMapper _mapper;
 
-        public ProductsController(IProductService productService)
+        public ProductsController(IProductService productService, ICategoryService categoryService, IMapper mapper)
         {
             _productService = productService;
+            _categoryService = categoryService;
+            _mapper = mapper;
         }
 
         public async Task<IActionResult> Index()
         {
             return View(await _productService.GetProductsWithCategory());
         }
+
+        public async Task<IActionResult> Save()
+        {
+            var categories = await _categoryService.GetAllAsync();
+
+            var categoriesDto = _mapper.Map<List<CategoryDto>>(categories.ToList());
+
+            ViewBag.categories = new SelectList(categoriesDto, "Id", "Name");
+
+            return View();
+
+        }
+        [HttpPost]
+        public async Task<IActionResult> Save(ProductDto productDto)
+        {
+           
+
+            if (ModelState.IsValid)
+            {
+                await _productService.AddAsync(_mapper.Map<Product>(productDto));
+                return RedirectToAction(nameof(Index));
+            }
+            var categories = await _categoryService.GetAllAsync();
+
+            var categoriesDto = _mapper.Map<List<CategoryDto>>(categories.ToList());
+
+            ViewBag.categories = new SelectList(categoriesDto, "Id", "Name");
+            return View();
+        }
+
     }
 }
