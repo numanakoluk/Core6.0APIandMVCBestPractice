2cd2cbff7f480270c443275f168c20aaac48f452
numanakoluk <numanakoluk01@gmail.com>
2022-03-23 18:09:21 +0300

    ProductApiService Methods

--- Web/Services/ProductApiService.cs
+++ Web/Services/ProductApiService.cs
@@ -1,4 +1,6 @@
-﻿namespace Web.Services
+﻿using Core.DTOs;
+
+namespace Web.Services
 {
     public class ProductApiService
     {
@@ -8,6 +10,48 @@
         {
             _httpClient = httpClient;
         }
+
+        public async Task<List<ProductWithCategoryDto>> GetProductsWithCategoryAsync()
+        {
+            //Take Jjson GetFromJson Methods
+            var response = await _httpClient.GetFromJsonAsync<CustomResponseDto<List<ProductWithCategoryDto>>>("products/GetProductsWithCategory");
+
+            return response.Data;
+        }
+        public async Task<ProductDto> GetByIdAsync(int id)
+        {
+
+            var response = await _httpClient.GetFromJsonAsync<CustomResponseDto<ProductDto>>($"products/{id}");
+            return response.Data;
+
+
+        }
+
+        public async Task<ProductDto> SaveAsync(ProductDto newProduct)
+        {
+            var response = await _httpClient.PostAsJsonAsync("products", newProduct);
+
+            if (!response.IsSuccessStatusCode) return null;
+
+            var responseBody = await response.Content.ReadFromJsonAsync<CustomResponseDto<ProductDto>>();
+
+            return responseBody.Data;
+
+
+        }
+        public async Task<bool> UpdateAsync(ProductDto newProduct)
+        {
+            var response = await _httpClient.PutAsJsonAsync("products", newProduct);
+
+            return response.IsSuccessStatusCode;
+        }
+        public async Task<bool> RemoveAsync(int id)
+        {
+            var response = await _httpClient.DeleteAsync($"products/{id}");
+
+            return response.IsSuccessStatusCode;
+        }
+
+
     }
 }
-
